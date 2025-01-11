6f5b5bc3406b267e82aa0270d2915546af932d85
Ravi <ravi@Ravis-MacBook-Air.local>
2025-01-11 19:32:22 +0545

    TYPESCRIPT, SHADCN AND SETUP

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,22 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
 
-// Define a TypeScript type for the song objects
-interface Song {
-  _id: string;
-  title: string;
-  artist: string;
-  duration: number; // duration in seconds
-}
 
-const App: React.FC = () => {
-  const [songs, setSongs] = useState<Song[]>([]); // Explicitly type the state
+function App() {
 
-  useEffect(() => {
-    const fetchSongs = async () => {
-      try {
-        const response = await axios.get<Song[]>(
-          "http://localhost:5000/api/songs"
-        );
-        setSongs(response.data);
-      } catch (error) {
-        console.error("Error fetching songs:", error);
-      }
-    };
-
-    fetchSongs();
-  }, []);
 
   return (
-    <div>
-      <h1>Welcome to Smoothie Plays</h1>
-      <h2>Song List:</h2>
-      <ul>
-        {songs.map((song) => (
-          <li key={song._id}>
-            {song.title} by {song.artist} ({Math.floor(song.duration / 60)}:
-            {song.duration % 60})
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    <>
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+    </>
 
-export default App;
+  )
+}
+
+export default App
