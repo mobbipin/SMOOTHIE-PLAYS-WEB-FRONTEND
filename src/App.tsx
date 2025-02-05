b8952a0c9f62a503d42b994967e79e7489b77ece
mobbipin <bipin@mobcreatives.com.np>
2025-02-05 10:19:31 +0545

    ADDED CARD AND CALLBACK FOR AUTH

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
-import { Route, Routes } from "react-router-dom";
-import MainLayout from "./layout/MainLayout";
-import AdminPage from "./pages/admin/AdminPage";
-import AlbumPage from "./pages/album/AlbumPage";
-import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
-import ChatPage from "./pages/chat/ChatPage";
-import HomePage from "./pages/home/HomePage";
-
 import { Toaster } from "react-hot-toast";
-
+import { Route, Routes } from "react-router-dom";
+import NotFoundPage from "./pages/404/NotFoundPage";
 
 function App() {
   return (
@@ -22,16 +15,10 @@ function App() {
             />
           }
         />
-        <Route path="/auth-callback" element={<AuthCallbackPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/chat" element={<ChatPage />} />
-          <Route path="/albums/:albumId" element={<AlbumPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
+        {/* Remaining */}
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
+
       <Toaster />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { ClerkProvider } from "@clerk/clerk-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./provider/AuthProvider.tsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
