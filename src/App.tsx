09cc6596830a9fc0c0ee21d085646f8ae2c18660
mobbipin <bipin@mobcreatives.com.np>
2025-02-05 09:34:20 +0545

    CREATED STORE AND PROVIDER FOR AUTH

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import AdminPage from "./pages/admin/AdminPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import ChatPage from "./pages/chat/ChatPage";
+import HomePage from "./pages/home/HomePage";
+
+import { Toaster } from "react-hot-toast";
 
 
 function App() {
-
-
   return (
     <>
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
-    </>
+      <Routes>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={"/auth-callback"}
+            />
+          }
+        />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/admin" element={<AdminPage />} />
 
-  )
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/albums/:albumId" element={<AlbumPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </>
+  );
 }
 
-export default App
+export default App;
