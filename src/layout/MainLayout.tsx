42602dc791b77bc393143b62718a204a3d04105d
mobbipin <bipin@mobcreatives.com.np>
2025-02-08 20:44:23 +0545

    ADDING LAYOUT FOR APP

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const MainLayout = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
+  return <></>;
+};
+export default MainLayout;
